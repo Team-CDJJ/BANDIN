c6e7f2d9a6b84a29d6c5cca2bb8233d07b1566c1
Ocknyer <kdu4045@naver.com>
2022-12-09 03:55:09 +0900

    chore: 초기 패키지 세팅

---
+++ src/styles/Globalstyled.jsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyled = createGlobalStyle`
+    ${reset}
+`;
+
+export default GlobalStyled;

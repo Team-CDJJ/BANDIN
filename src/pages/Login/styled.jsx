8d7d62f7cca43c1c81f65d034c5c2433a10c423c
Ocknyer <kdu4045@naver.com>
2022-12-13 17:38:11 +0900

    feat: 스플래쉬 페이지 임시 구현

--- src/pages/Login/styled.jsx
+++ src/pages/Login/styled.jsx
@@ -1,74 +1,87 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const LoginPageWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: #7143df;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  ${({ theme }) => {
+    return css`
+      width: 100vw;
+      height: 100vh;
+      background-color: ${theme.colors.mainColor};
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-end;
 
-  .logo-white {
-    width: 120px;
-    margin-top: 204px;
-  }
+      .logo-white {
+        width: 120px;
+        margin-bottom: 180px;
+      }
+    `;
+  }}
 `;
 
 export const LoginBtnContainer = styled.section`
+  display: flex;
+  flex-direction: column;
   padding: 50px 34px;
   background-color: #fff;
   border-radius: 20px 20px 0 0;
   width: 100%;
-  position: absolute;
-  bottom: 0;
 `;
 
 export const BtnSocialLogin = styled.button`
-  width: 100%;
-  background-color: transparent;
-  line-height: 42px;
-  padding: 0px 14px;
-  border-radius: 20px;
-  border: none;
-  position: relative;
-  font-size: 14px;
-  margin-bottom: 10px;
-  color: #767676;
+  ${({ theme }) => {
+    return css`
+      width: 100%;
+      background-color: transparent;
+      line-height: 42px;
+      padding: 0px 14px;
+      border-radius: 20px;
+      border: none;
+      position: relative;
+      font-size: ${theme.fontSizes.md};
+      margin-bottom: 10px;
+      color: ${theme.colors.grey76};
 
-  > img {
-    width: 24px;
-    position: absolute;
-    top: 50%;
-    left: 14px;
-    transform: translate(0, -50%);
-  }
+      > img {
+        width: 24px;
+        position: absolute;
+        top: 50%;
+        left: 14px;
+        transform: translate(0, -50%);
+      }
 
-  &:nth-child(1) {
-    border: 1px solid #f2c94c;
-  }
+      &:nth-child(1) {
+        border: 1px solid #f2c94c;
+      }
 
-  &:nth-child(2) {
-    border: 1px solid #767676;
-  }
+      &:nth-child(2) {
+        border: 1px solid ${theme.colors.grey76};
+      }
 
-  &:nth-child(3) {
-    border: 1px solid #2d9cdb;
-    margin-bottom: 20px;
-  }
+      &:nth-child(3) {
+        border: 1px solid #2d9cdb;
+        margin-bottom: 20px;
+      }
+    `;
+  }}
 `;
 
 export const LinkWrapper = styled.div`
-  font-size: 12px;
-  text-align: center;
+  ${({ theme }) => {
+    return css`
+      font-size: ${theme.fontSizes.sm};
+      text-align: center;
 
-  .link-to {
-    text-decoration: none;
-    color: #767676;
+      .link-to {
+        text-decoration: none;
+        color: ${theme.colors.grey76};
 
-    &:first-child::after {
-      content: '|';
-      color: #c4c4c4;
-      margin: 0 13px;
-    }
-  }
+        &:first-child::after {
+          content: '|';
+          color: #c4c4c4;
+          margin: 0 13px;
+        }
+      }
+    `;
+  }}
 `;
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const fontSizes = {
+  xs: '10px',
+  sm: '12px',
+  md: '14px',
+  lg: '16px',
+  xl: '18px',
+  xxl: '24px',
+};
+
+const colors = {
+  greyF2: '#F2F2F2',
+  grey76: '#767676',
+  greyDB: '#DBDBDB',
+  mainColor: '#7143DF',
+  mainDisabled: '#BDABE8',
+  errorRed: '#EB5757',
+};
+
+const lightTheme = {
+  fontSizes,
+  colors,
+};
+
+export default lightTheme;

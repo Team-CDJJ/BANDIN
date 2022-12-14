11f50bda7dd32ceebe5abb4b0396134095ab0651
Ocknyer <kdu4045@naver.com>
2022-12-15 01:53:32 +0900

    fix app.jsx 충돌수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 // import Home from './pages/Home/Home';
 import EmailLogin from './pages/EmailLogin/EmailLogin';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
-// import Home from './pages/Home/Home';
-import Following from './pages/Following/Following';
-import DeleteAlert from './components/CommonUI/DeleteAlert/DeleteAlert';
-import UserItem from './components/CommonUI/UserItem/UserItem';
+import SetUpProfile from './pages/SetUpProfile/SetUpProfile';
 
 const App = () => {
   return (
@@ -16,9 +14,7 @@ const App = () => {
         <Route path='/' element={<Login />} />
         <Route path='/emaillogin' element={<EmailLogin />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/following' element={<Following />} />
-        <Route path='/deletealert' element={<DeleteAlert />} />
-        <Route path='/useritem' element={<UserItem />} />
+        <Route path='/setupprofile' element={<SetUpProfile />} />
       </Routes>
     </BrowserRouter>
   );

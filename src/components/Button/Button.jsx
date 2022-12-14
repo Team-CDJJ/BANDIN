d0588fb24ed7cbdcb65258c43f2f878839ff4347
Ocknyer <kdu4045@naver.com>
2022-12-15 02:01:25 +0900

    fix: Button.jsx 충돌 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,5 @@
-import styled, { css } from 'styled-components';
+// import styled from "styled-components"
 
-export const Button1 = styled.button`
-  ${({ theme }) => {
-    return css`
-      width: 100%;
-      background-color: ${theme.colors.mainColor};
-      color: #ffffff;
-      padding: 13px 0;
-      border: none;
-      border-radius: 44px;
-      margin-bottom: 20px;
-      &:disabled {
-        background-color: ${theme.colors.mainDisabled};
-      }
-    `;
-  }}
-`;
+// const Button = styled.button`
+
+// `

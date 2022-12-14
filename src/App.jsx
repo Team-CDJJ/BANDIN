e62910bad68c4968f1101cc5c4810f41bc460268
Ocknyer <kdu4045@naver.com>
2022-12-15 01:55:37 +0900

    fix: App,jsx 충돌 수정

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// import Home from './pages/Home/Home';
+import EmailLogin from './pages/EmailLogin/EmailLogin';
+import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
+import SetUpProfile from './pages/SetUpProfile/SetUpProfile';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/' element={<Login />} />
+        <Route path='/emaillogin' element={<EmailLogin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/setupprofile' element={<SetUpProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;

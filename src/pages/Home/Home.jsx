977109b4e84df67aa9de9c774b4487c48fadae24
youngjun.choi <dlfgudwns12@gmail.com>
2022-12-17 15:58:16 +0900

    feat: 피드 매칭_페이지 마크업 nav 수정 전

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import TopMainNav from '../../components/CommonUI/Nav/TopMainNav/TopMainNav';
+import TapNav from '../../components/CommonUI/TabMenu/TabMenu';
+import { EmptyHomeWrap, HomeWrap, FeedTxt } from './styled';
+// import { Link } from 'react-router-dom';
 // import Splash from '../Splash/Splash';
+import Post from '../../components/CommonUI/CommonPost/CommonPost';
+import TxtPost from '../../components/CommonUI/CommonOnlyText/CommonOnlyText';
+import profileImg from '../../assets/profile.png';
+import Button from '../../components/atoms/Button/Button';
 
 const Home = () => {
+  const [post, setPost] = useState(false);
   return (
-    <div>
-      <Link to='/login'>로그인</Link>
-    </div>
+    <>
+      <TopMainNav />
+      {post ? (
+        <HomeWrap>
+          <Post userName='' userId='' />
+          <Post userName='' userId='' />
+          <TxtPost userName='' userId='' />
+        </HomeWrap>
+      ) : (
+        <EmptyHomeWrap>
+          <img src={profileImg} alt='프로필 이미지' className='profile-img' />
+          <FeedTxt>유저를 검색해 팔로우 해보세요!</FeedTxt>
+          <Button type='button' size='md, lg' tit='검색하기'></Button>
+          {/* <Link to='/login'>로그인</Link> */}
+        </EmptyHomeWrap>
+      )}
+      <TapNav />;
+    </>
   );
 };
 
---
+++ src/pages/Home/styled.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from 'styled-components';
+
+export const EmptyHomeWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 210px auto 0;
+
+  .profile-img {
+    width: 110px;
+    height: 110px;
+  }
+`;
+
+export const HomeWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 88px 16px 0;
+  gap: 10px;
+`;
+
+export const FeedTxt = styled.span`
+  ${({ theme }) => {
+    return css`
+      font-size: ${theme.fontSizes.smd};
+      color: ${theme.colors.grey76};
+      line-height: 14px;
+      margin: 20px 0;
+    `;
+  }}
+`;
+
+// export const Button = styled.button`
+//   width: 120px;
+// `;

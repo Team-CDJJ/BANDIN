b11d29fa0ada916031c4dee6ea356810a4c8d4f0
Ocknyer <kdu4045@naver.com>
2022-12-27 18:24:52 +0900

    fix: 캐시 파일 삭제

--- src/api/profile/getmyprofile.js
+++
@@ -1,8 +0,0 @@
-import { accessInstance } from '../api';
-
-const getMyProfile = async (accountname) => {
-  const res = await accessInstance.get(`/profile/${accountname}`);
-  return res.data.profile;
-};
-
-export default getMyProfile;

b5e3c760903c8da2773d355a9a30ecb898029b26
Ocknyer <kdu4045@naver.com>
2022-12-29 16:02:10 +0900

    feat: 게시물 수정 기능 구현

---
+++ src/pages/EditPost/styled.jsx
@@ -0,0 +1,77 @@
+import styled, { css } from 'styled-components';
+import deleteIcon from '../../assets/icon-delete.png';
+
+export const UploadWrapper = styled.main`
+  margin-top: 44px;
+  width: 100%;
+  padding: 20px 16px;
+  display: flex;
+  overflow: hidden;
+
+  > img {
+    width: 42px;
+  }
+
+  .upload-form {
+    flex-grow: 1;
+  }
+
+  > label {
+    right: 16px;
+    bottom: 16px;
+  }
+`;
+
+export const StyledTextArea = styled.textarea`
+  ${({ theme }) => {
+    return css`
+      width: 100%;
+      border: none;
+      resize: none;
+      margin: 12px;
+
+      &::placeholder {
+        font-size: ${theme.colors.md}
+        line-height: 18px;
+        color: ${theme.colors.grayDB};
+      }
+      &:focus {
+        outline: none;
+      }
+    `;
+  }}
+`;
+
+export const ImgBox = styled.ul`
+  ${({ theme }) => {
+    return css`
+      width: 300px;
+      display: flex;
+      gap: 10px;
+      margin-top: 10px;
+      overflow-x: scroll;
+
+      .img-list {
+        position: relative;
+        > img {
+          border-radius: ${theme.borderRadius.lv2};
+        }
+
+        .delete-btn {
+          position: absolute;
+          top: 6px;
+          right: 6px;
+          > img {
+            width: 22px;
+            height: 22px;
+          }
+          /* width: 22px;
+          height: 22px;
+          top: 6px;
+          right: 6px;
+          background: url(${deleteIcon}) no-repeat center / 22px 22px; */
+        }
+      }
+    `;
+  }}
+`;

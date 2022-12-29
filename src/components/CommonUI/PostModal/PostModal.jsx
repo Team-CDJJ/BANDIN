42066e6a890e7f40622dd2583884f6b91e0e5ed5
youngjun.choi <dlfgudwns12@gmail.com>
2022-12-30 00:48:25 +0900

    feat: TopMainNav 햄버거 버튼 클릭 시 모달 기능 구현 완료

--- src/components/CommonUI/PostModal/PostModal.jsx
+++ src/components/CommonUI/PostModal/PostModal.jsx
@@ -11,7 +11,11 @@ import {
   AlertTxt,
   BtnsWrapper,
   ModalBtn,
+  ProductModalArticle,
+  ProductModalText,
+  ModalBtnWrapper,
 } from './styled';
+import DeleteAlert from '../DeleteAlert/DeleteAlert';
 
 export const PostChatModal = ({ leaveChatRoom }) => {
   // 채팅방 나가기
@@ -33,6 +37,41 @@ export const CommentModal = ({ content, onClickDelete }) => {
       <GrayLine />
       <CommentModalText onClick={onClickDelete}>{content}</CommentModalText>
     </CommentModalContainer>
+  );
+};
+
+export const ProductModal = ({ productId, setModal, productLink }) => {
+  const [onAlert, setOnAlert] = useState(false);
+  const AlertOpen = () => {
+    setOnAlert(!onAlert);
+  };
+  const handleCancel = () => {
+    setOnAlert(false);
+    setModal(false);
+  };
+  const handleCloseModal = () => {
+    setModal(false);
+  };
+
+  return (
+    <>
+      <ProductModalArticle onClick={handleCloseModal}>
+        <h2 className='ir'>모달창</h2>
+        <GrayLine />
+        <ModalBtnWrapper type='button' onClick={(e) => e.stopPropagation()}>
+          <ProductModalText onClick={AlertOpen}>삭제</ProductModalText>
+          <Link to={`/product/${productId}/modify`}>
+            <ProductModalText>수정</ProductModalText>
+          </Link>
+          <a href={productLink} target='_blank' rel='noopener noreferrer'>
+            <ProductModalText>웹사이트에서 상품 보기</ProductModalText>
+          </a>
+        </ModalBtnWrapper>
+      </ProductModalArticle>
+      {onAlert && (
+        <DeleteAlert productId={productId} handleCancel={handleCancel} />
+      )}
+    </>
   );
 };
 
@@ -68,12 +107,12 @@ export const PostVerticalModal = ({ setModalOpen }) => {
         <CommentModalText onClick={AlertOpen}>삭제</CommentModalText>
         <CommentModalText>수정</CommentModalText>
       </PostModalArticle>
-      {onAlert && <DeleteAlert />}
+      {onAlert && <DeletePost />}
     </ModalBackGround>
   );
 };
 
-export const DeleteAlert = () => {
+export const DeletePost = () => {
   return (
     <AlertWrapper>
       <AlertTxt>상품을 삭제할까요?</AlertTxt>
@@ -82,5 +121,48 @@ export const DeleteAlert = () => {
         <ModalBtn type='button'>삭제</ModalBtn>
       </BtnsWrapper>
     </AlertWrapper>
+  );
+};
+
+export const NavVerticalModal = ({ setModalOpen }) => {
+  const [onAlert, setOnAlert] = useState(false);
+  const navigate = useNavigate();
+
+  // 모달 외부 클릭 시 끄기 처리
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setModalOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  });
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('accountname');
+    localStorage.removeItem('recoil-persist');
+    navigate('/');
+    window.location.reload();
+  };
+
+  return (
+    <ModalBackGround>
+      <PostModalArticle ref={modalRef}>
+        <h2 className='ir'>모달창</h2>
+        <GrayLine />
+        <CommentModalText>설정 및 개인정보</CommentModalText>
+        <CommentModalText onClick={handleLogout}>로그아웃</CommentModalText>
+      </PostModalArticle>
+      {onAlert && <DeletePost />}
+    </ModalBackGround>
   );
 };

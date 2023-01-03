f5b652dc85fab70866a8180b77b1fe2ffb74c67b
youngjun.choi <dlfgudwns12@gmail.com>
2023-01-03 12:54:41 +0900

    feat: 무한스크롤 라이브러리 설치 및 API 연결

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
+import axios from 'axios';
 import TopMainNav from '../../components/CommonUI/Nav/TopMainNav/TopMainNav';
 import { EmptyHomeWrapper, FeedWrapper, FeedTxt } from './styled';
 import CommonPost from '../../components/CommonUI/CommonPost/CommonPost';
 import profileImg from '../../assets/profile.png';
 import Button from '../../components/atoms/Button/Button';
 import TabMenu from '../../components/CommonUI/TabMenu/TabMenu';
-import getFeedPost from '../../api/post/getFeedPost';
+// import getFeedPost from '../../api/post/getFeedPost';
 
 const Home = () => {
   const [postData, setPostData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const userToken = localStorage.getItem('token');
+  const [numFeed, setNumFeed] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [done, setDone] = useState(false);
+  const [ref, inView] = useInView();
+
+  const getFeedPost = useCallback(async () => {
+    console.log(userToken);
+    // const url = 'https://mandarin.api.weniv.co.kr';
+    const option = {
+      url: `https://mandarin.api.weniv.co.kr/post/feed/?limit=10&skip=${numFeed}`,
+      method: `GET`,
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-type': 'application/json',
+      },
+    };
+    setLoading(true);
+    await axios(option)
+      .then((res) => {
+        setPostData(postData.concat(res.data.posts));
+        setLoading(false);
+        setIsLoading(false);
+        if (res.data.posts.length < 10) {
+          setDone(true);
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.error(err);
+      });
+  }, [numFeed]);
 
   useEffect(() => {
-    getFeedPost()
-      .then((data) => {
-        setPostData(data.posts);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    if (!done) {
+      getFeedPost();
+    }
+  }, [numFeed]);
+
+  useEffect(() => {
+    if (inView && !loading) {
+      setNumFeed((current) => current + 10);
+    }
+  }, [inView, loading]);
 
   return (
     <>
@@ -27,9 +64,15 @@ const Home = () => {
       {postData.length !== 0 ? (
         <FeedWrapper>
           <h2 className='ir'>홈 피드</h2>
-          {postData.map((post, idx) => {
-            return <CommonPost key={idx} post={post} />;
-          })}
+          {postData.map((post, idx) =>
+            postData.length - 1 === idx ? (
+              <div key={post.id} ref={ref} />
+            ) : (
+              <div key={post.id}>
+                <CommonPost post={post} />
+              </div>
+            ),
+          )}
         </FeedWrapper>
       ) : (
         <EmptyHomeWrapper>

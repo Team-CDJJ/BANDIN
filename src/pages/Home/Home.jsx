65b670d184611fa18b9e0637e65361ec48bb1529
youngjun.choi <dlfgudwns12@gmail.com>
2023-01-04 23:58:42 +0900

    feat: 홈 피드 로딩페이지 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 import axios from 'axios';
 import TopMainNav from '../../components/CommonUI/Nav/TopMainNav/TopMainNav';
-import { EmptyHomeWrapper, FeedWrapper, FeedTxt, TopBtn } from './styled';
+import {
+  EmptyHomeWrapper,
+  FeedWrapper,
+  FeedTxt,
+  TopBtn,
+  LoadingTxt,
+  LoadingWrapper,
+} from './styled';
 import CommonPost from '../../components/CommonUI/CommonPost/CommonPost';
 import profileImg from '../../assets/profile.png';
 import topImg from '../../assets/top-btn.png';
@@ -18,6 +25,7 @@ const Home = () => {
   const [numFeed, setNumFeed] = useState(0);
   const [done, setDone] = useState(false);
   const [ref, inView] = useInView();
+  const [view, setView] = useState('pending');
 
   const getFeedPost = useCallback(async () => {
     const option = {
@@ -32,12 +40,14 @@ const Home = () => {
     await axios(option)
       .then((res) => {
         setPostData(postData.concat(res.data.posts));
+        setView('fulfilled');
         if (res.data.posts.length < 10) {
           setDone(true);
         }
       })
       .catch((err) => {
         console.error(err);
+        setView('rejected');
       });
   }, [numFeed]);
 
@@ -63,41 +73,48 @@ const Home = () => {
   return (
     <>
       <TopMainNav />
-      {postData.length !== 0 ? (
-        <FeedWrapper>
-          <h2 className='ir'>홈 피드</h2>
-          {postData.map((post, i) =>
-            postData.length - 1 === i ? (
-              <div key={post.id} ref={ref}>
-                <CommonPost post={post} />
+      {view === 'fulfilled' ? (
+        postData.length !== 0 ? (
+          <FeedWrapper>
+            <h2 className='ir'>홈 피드</h2>
+            {postData.map((post, i) =>
+              postData.length - 1 === i ? (
+                <div key={post.id} ref={ref}>
+                  <CommonPost post={post} />
+                </div>
+              ) : (
+                <div key={post.id}>
+                  <CommonPost post={post} />
+                </div>
+              ),
+            )}
+            <TopBtn>
+              <div>
+                <button onClick={scrollToTop} type='button'>
+                  <Img src={topImg} alt='위 방향 화살표' />
+                </button>
               </div>
-            ) : (
-              <div key={post.id}>
-                <CommonPost post={post} />
-              </div>
-            ),
-          )}
-
-          <TopBtn>
-            <div>
-              <button onClick={scrollToTop} type='button'>
-                <Img src={topImg} alt='위 방향 화살표' />
-              </button>
-            </div>
-          </TopBtn>
-        </FeedWrapper>
+            </TopBtn>
+          </FeedWrapper>
+        ) : (
+          <EmptyHomeWrapper>
+            <h2 className='ir'>홈 피드</h2>
+            <img src={profileImg} alt='프로필 이미지' className='profile-img' />
+            <FeedTxt>유저를 검색해 팔로우 해보세요!</FeedTxt>
+            <Link to='/search'>
+              <Button type='button' size='md, lg' tit='검색하기'></Button>
+            </Link>
+          </EmptyHomeWrapper>
+        )
       ) : (
-        <EmptyHomeWrapper>
-          <h2 className='ir'>홈 피드</h2>
-          <img src={profileImg} alt='프로필 이미지' className='profile-img' />
-          <FeedTxt>유저를 검색해 팔로우 해보세요!</FeedTxt>
-          <Link to='/search'>
-            <Button type='button' size='md, lg' tit='검색하기'></Button>
-          </Link>
-        </EmptyHomeWrapper>
+        <LoadingWrapper>
+          <Img src={profileImg} alt='프로필 이미지' className='loading-img' />
+          <LoadingTxt>빠르게 로딩중!!</LoadingTxt>
+        </LoadingWrapper>
       )}
       <TabMenu place='homefeed' />
     </>
   );
 };
+
 export default Home;
--- src/pages/Home/styled.jsx
+++ src/pages/Home/styled.jsx
@@ -46,3 +46,27 @@ export const TopBtn = styled.div`
     cursor: pointer;
   }
 `;
+
+export const LoadingTxt = styled.span`
+  ${({ theme }) => {
+    return css`
+      font-size: ${theme.fontSizes.xl};
+      color: ${theme.colors.grey76};
+      line-height: 14px;
+      margin: 20px 0;
+      font-weight: 500; ;
+    `;
+  }}
+`;
+
+export const LoadingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 310px auto 0;
+
+  .loading-img {
+    width: 110px;
+    height: 110px;
+  }
+`;

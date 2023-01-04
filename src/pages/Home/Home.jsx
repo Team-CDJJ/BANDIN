fc360f88a8c3c8ba0c33f32351d90905a163d40e
annasyun <yunjbly2470@naver.com>
2023-01-04 13:46:53 +0900

    feat: 로딩화면 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-nested-ternary */
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import TopMainNav from '../../components/CommonUI/Nav/TopMainNav/TopMainNav';
-import { EmptyHomeWrapper, FeedWrapper, FeedTxt } from './styled';
+import { EmptyHomeWrapper, FeedWrapper, FeedTxt, LoadingTxt } from './styled';
 import CommonPost from '../../components/CommonUI/CommonPost/CommonPost';
 import profileImg from '../../assets/profile.png';
 import Button from '../../components/atoms/Button/Button';
@@ -10,28 +11,31 @@ import getFeedPost from '../../api/post/getFeedPost';
 
 const Home = () => {
   const [postData, setPostData] = useState([]);
+  const [view, setView] = useState('pending');
 
   useEffect(() => {
     getFeedPost()
       .then((data) => {
         setPostData(data.posts);
+        setView('fulfilled');
       })
       .catch((error) => {
         console.log(error);
+        setView('rejected');
       });
   }, []);
 
   return (
     <>
       <TopMainNav />
-      {postData.length !== 0 ? (
+      {view === 'fulfilled' && postData.length !== 0 ? (
         <FeedWrapper>
           <h2 className='ir'>홈 피드</h2>
           {postData.map((post, idx) => {
             return <CommonPost key={idx} post={post} />;
           })}
         </FeedWrapper>
-      ) : (
+      ) : view === 'fulfilled' && postData.length === 0 ? (
         <EmptyHomeWrapper>
           <h2 className='ir'>홈 피드</h2>
           <img src={profileImg} alt='프로필 이미지' className='profile-img' />
@@ -39,6 +43,15 @@ const Home = () => {
           <Link to='/search'>
             <Button type='button' size='md, lg' tit='검색하기'></Button>
           </Link>
+        </EmptyHomeWrapper>
+      ) : (
+        <EmptyHomeWrapper>
+          <img
+            src={profileImg}
+            alt='프로필 이미지'
+            className='profile-img loading-img'
+          />
+          <LoadingTxt>로딩중!!!</LoadingTxt>
         </EmptyHomeWrapper>
       )}
       <TabMenu place='homefeed' />
